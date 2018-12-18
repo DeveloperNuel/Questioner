a5affb3341eaae7a8b58dfc39e388b8bb7e20e68
DeveloperNuel <egentle05@gmail.com>
2018-12-18 22:22:06 +0200

    [Feature 162654070] Create Sign Up Page.

---
+++ UI/js/signup.js
@@ -0,0 +1,8 @@
+
+document.getElementById('register').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'flex';
+});
+
+document.querySelector('.close2').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'none';
+});
