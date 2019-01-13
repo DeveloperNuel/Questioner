e017aac72f8aac232bab0649607fbd5054ed5dc3
DeveloperNuel <egentle05@gmail.com>
2019-01-14 01:06:36 +0200

    Change Port.

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use((req, res) => {
   res.status(404).send('Resource Not Found');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
