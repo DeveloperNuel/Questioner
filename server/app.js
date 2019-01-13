2eeefd315cf1c088afc24b69923e302481c00dcd
DeveloperNuel <egentle05@gmail.com>
2019-01-14 00:03:09 +0200

    Change Port number.

--- server/app.js
+++ server/app.js
@@ -12,13 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Use routes
 app.use('/api/v1/meetup', meetups);
+app.use('/api/v1/questions', meetups);
 // app.use('/api/v1/user', users);
 // Handler for 404 - Resource Not Found
 app.use((req, res) => {
   res.status(404).send('Resource Not Found');
 });
 
-const PORT = 5000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
