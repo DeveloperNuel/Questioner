4b8ab94528185c955698828a4577c20907071d87
DeveloperNuel <egentle05@gmail.com>
2019-01-16 01:55:38 +0200

    [Chore #163243057] Apply LFA's Feedback.

---
+++ UI/js/profile.js
@@ -0,0 +1,9 @@
+function edit(el) {
+  el.childNodes[0].removeAttribute('disabled');
+  el.childNodes[0].focus();
+  window.getSelection().removeAllRanges();
+}
+function disable(el) {
+  el.setAttribute('disabled', '');
+}
+ 
